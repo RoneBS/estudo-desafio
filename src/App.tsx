b0ebd13b1ec5528953fcf3c0b201e18d8ab5d608
Rone Silveira <ronebs10@hotmail.com>
2023-01-26 15:51:04 -0300

    refactor: removed list contents, styles and types, added card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,8 @@
-import { useEffect, useState } from "react"
-import { api } from "./services/api"
-import { PopularMovie } from "./type"
- 
-const imgMediumPath = import.meta.env.VITE_MEDIUM_IMG_PATH
-
-const apiKey = import.meta.env.VITE_API_KEY
-
-import './styles/styles.css'
+import { Card } from "./components/Card"
 
 function App() {
-  const [popularMovies, setPopularMovies] = useState<PopularMovie[]>([])
-
-  async function getPopularMovies() {
-    const {data} = await api.get(`popular?api_key=${apiKey}`)
-    const formattedMovies = data.results.map((movie:PopularMovie) => ({
-        ...movie, 
-        image: `${imgMediumPath}${movie.poster_path}`
-      }
-    ))
-    
-    setPopularMovies(formattedMovies)
-  }
-  
-  useEffect(() => {
-    getPopularMovies()
-  },[])
-
   return (
-   <div className="container">
-    <div className="wrap">
-      {popularMovies.map(movie => (
-        <div className="card" key={movie.id}>
-          <img src={movie.image} alt={movie.title} />
-          <strong>{movie.title}</strong>
-        </div>
-      ))}
-    </div>
-   </div>
+   <Card />
   )
 }
 

5fbd261d52125eb025c187f8c97d1d068411cdbb
Rone Silveira <ronebs10@hotmail.com>
2023-01-26 00:17:28 -0300

    feat: created method to get popular movies from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react"
+import { api } from "./services/api"
+import { PopularMovie } from "./type"
+ 
+const imgMediumPath = import.meta.env.VITE_MEDIUM_IMG_PATH
+
+const apiKey = import.meta.env.VITE_API_KEY
+
 function App() {
+  const [popularMovies, setPopularMovies] = useState<PopularMovie[]>([])
+
+  async function getPopularMovies() {
+    const {data} = await api.get(`popular?api_key=${apiKey}`)
+    const formattedMovies = data.results.map((movie:PopularMovie) => ({
+        ...movie, 
+        image: `${imgMediumPath}${movie.poster_path}`
+      }
+    ))
+    
+    setPopularMovies(formattedMovies)
+  }
+  
+  useEffect(() => {
+    getPopularMovies()
+  },[])
+
   return (
-    <h1>Hello world</h1>
+   <>
+   {popularMovies.map(movie => (
+     <div key={movie.id}>
+      <img src={movie.image} alt={movie.title} />
+      <strong>{movie.title}</strong>
+     </div>
+   ))}
+   </>
   )
 }
 

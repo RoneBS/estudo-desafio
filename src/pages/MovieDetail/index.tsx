519114aa5ef6cedc8e43a6daa41a1b5eb604e930
Rone Silveira <ronebs10@hotmail.com>
2023-02-01 15:25:35 -0300

    feat: function created that requests movie details by id, and the types

--- src/pages/MovieDetail/index.tsx
+++ src/pages/MovieDetail/index.tsx
@@ -1,3 +1,23 @@
+import { api } from '@/services/api'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const apiKey = import.meta.env.VITE_API_KEY
+
 export function MovieDetail() {
+  const [movieDetails, setMovieDetails] = useState([])
+  const movieId = useParams()
+  const { id } = movieId
+
+  async function GetMovieDetails() {
+    const { data } = await api.get(`${id}?api_key=${apiKey}`)
+    console.log(data)
+    setMovieDetails(data)
+  }
+
+  useEffect(() => {
+    GetMovieDetails()
+  }, [])
+
   return <h1 style={{ color: 'white' }}>Movie detail</h1>
 }

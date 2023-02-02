44414d4c9064913a815a1b6fe55cb15137b957de
Rone Silveira <ronebs10@hotmail.com>
2023-02-02 00:41:56 -0300

    refactor: add object and remove array form useState hook, add useCallback

--- src/pages/MovieDetail/index.tsx
+++ src/pages/MovieDetail/index.tsx
@@ -1,23 +1,45 @@
 import { api } from '@/services/api'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { MovieDetailProps } from './type'
 
 const apiKey = import.meta.env.VITE_API_KEY
+const imgLargePath = import.meta.env.VITE_LARGE_IMG_PATH
 
 export function MovieDetail() {
-  const [movieDetails, setMovieDetails] = useState([])
+  const [movieDetails, setMovieDetails] = useState<MovieDetailProps>(
+    {} as MovieDetailProps
+  )
+
   const movieId = useParams()
   const { id } = movieId
 
-  async function GetMovieDetails() {
+  const GetMovieDetails = useCallback(async () => {
     const { data } = await api.get(`${id}?api_key=${apiKey}`)
+
     console.log(data)
     setMovieDetails(data)
-  }
+  }, [id])
 
   useEffect(() => {
     GetMovieDetails()
-  }, [])
+  }, [GetMovieDetails])
 
-  return <h1 style={{ color: 'white' }}>Movie detail</h1>
+  return (
+    <div>
+      <h1 style={{ color: 'white' }}>Movie detail</h1>
+      <div>
+        {movieDetails && (
+          <div>
+            <p style={{ color: 'white' }}>{movieDetails.title}</p>
+            <img
+              src={`${imgLargePath}${movieDetails.poster_path}`}
+              alt={movieDetails.title}
+            />
+            <p style={{ color: 'white' }}>{movieDetails.overview}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }

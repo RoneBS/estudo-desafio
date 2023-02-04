230e0d684ebb0d4c4107e4ba5ec44ec8a43fabd5
Rone Silveira <ronebs10@hotmail.com>
2023-02-04 14:53:08 -0300

    feat: add debounce function on search input

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,33 @@
 import { Link } from 'react-router-dom'
 import { FilmSlate, MagnifyingGlass } from 'phosphor-react'
+import { ChangeEvent, useState } from 'react'
+import { useDebouncedCallback } from 'use-debounce'
 
 export function Header() {
+  const [search, setSearch] = useState('')
+
+  const debounced = useDebouncedCallback((search) => {
+    setSearch(search)
+  }, 500)
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    debounced(e.target.value)
+  }
+  console.log(search)
   return (
     <header>
       <nav>
         <Link to={'/'}>
+          <p>Filmes</p>
           <FilmSlate weight="fill" style={{ color: 'white' }} />
         </Link>
 
-        <input type="text" placeholder="buscar" />
+        <input
+          type="text"
+          placeholder="buscar"
+          defaultValue={''}
+          onChange={handleChange}
+        />
         <MagnifyingGlass />
       </nav>
     </header>

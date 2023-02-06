7f2ab0f07135d482991e5cf5de813a43386c5288
Rone Silveira <ronebs10@hotmail.com>
2023-02-06 18:01:43 -0300

    style: add initial style to Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import { styled } from '@stitches/react'
+
+export const Container = styled('header', {
+  maxWidth: '100%'
+})
+
+export const ModalContainer = styled('div', {
+  height: '340px',
+  overflow: 'scroll',
+  position: 'absolute',
+  background: 'white',
+  zIndex: '1'
+})

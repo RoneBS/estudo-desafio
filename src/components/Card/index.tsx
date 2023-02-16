2f98bf9bf4cf8d2ba76ee2e6e9473ba769edc32c
Rone Silveira <ronebs10@hotmail.com>
2023-02-15 21:57:39 -0300

    style: add style to card, change card dimension

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,14 +1,19 @@
 import { PopularMovie } from '@/pages/Home/type'
+import { Star } from 'phosphor-react'
 
 import * as S from './styles'
 import { Link } from 'react-router-dom'
 
-export function Card({ id, image, title }: PopularMovie) {
+export function Card({ id, image, title, vote_count }: PopularMovie) {
   return (
     <Link to={`/movie-detail/${id}`}>
       <S.Card>
         <S.MovieCover src={image} alt={title} />
         <S.Text>{title}</S.Text>
+        <S.Text>
+          <Star />
+          {vote_count}
+        </S.Text>
       </S.Card>
     </Link>
   )
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,22 +1,26 @@
 import { styled } from '@stitches/react'
 
 export const Card = styled('div', {
+  width: '9.625rem',
+  height: '22.5rem',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  border: '2px solid #fede00',
+  // border: '2px solid #fede00',
+  borderRadius: '8px',
   background: '#000',
   color: '#fff',
-  padding: '0.5rem',
-  margin: '0.5rem'
+  // padding: '0.5rem',
+  margin: '2.5rem'
 })
 
 export const MovieCover = styled('img', {
   maxWidth: 'fit-content'
+  // borderRadius: '8px'
 })
 
 export const Text = styled('strong', {
-  width: '10cm',
+  width: '4cm',
   padding: '0.5rem',
   textAlign: 'center'
 })

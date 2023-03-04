cf8659ef2c40d8b8007a1806e4a65bb612bf70ef
Rone Silveira <ronebs10@hotmail.com>
2023-03-03 21:57:34 -0300

    refactor: remove stitches and  add to Header styled components syntax

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,47 +1,47 @@
-import { styled } from '@stitches/react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-export const Container = styled('header', {
-  maxWidth: '100%',
-  height: '5rem',
-  background: '#000'
-})
+export const Container = styled.header`
+  max-width: 100%;
+  height: 5rem;
+  background: #000;
+`
 
-export const Navbar = styled('nav', {
-  height: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  padding: '1.5rem'
-})
+export const Navbar = styled.nav`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1.5rem;
+`
 
-export const LinkContainer = styled(Link, {
-  display: 'flex',
-  alignItems: 'center',
-  height: '100%',
-  textDecoration: 'none',
+export const LinkContainer = styled(Link)`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  text-decoration: none;
 
-  p: {
-    marginLeft: '10px',
-    fontFamily: 'Helvetica, sans-serif',
-    color: 'white',
-    fontWeight: 600
+  p {
+    margin-left: 10px;
+    font-family: Helvetica, sans-serif;
+    color: white;
+    font-weight: 600;
   }
-})
+`
 
-export const InputHeader = styled('input', {
-  width: '20rem',
-  height: '2.2rem',
-  border: '2px solid yellow',
-  borderRadius: '8px',
-  background: 'gray',
-  color: 'white'
-})
+export const InputHeader = styled.input`
+  width: 20rem;
+  height: 2.2rem;
+  border: 2px solid yellow;
+  border-radius: 8px;
+  background: gray;
+  color: white;
+`
 
-export const ModalContainer = styled('div', {
-  height: '340px',
-  overflow: 'scroll',
-  position: 'absolute',
-  background: 'white',
-  zIndex: '1'
-})
+export const ModalContainer = styled.div`
+  height: 340px;
+  overflow: scroll;
+  position: absolute;
+  background: white;
+  z-index: 1;
+`

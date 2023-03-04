ebad572cf515da008175c4ae9847579b3a0e2998
Rone Silveira <ronebs10@hotmail.com>
2023-03-03 22:00:44 -0300

    refactor: remove stitches and  add to HeaderModal styled components syntax

--- src/components/Header/components/HeaderModal/styles.ts
+++ src/components/Header/components/HeaderModal/styles.ts
@@ -1,22 +1,22 @@
-import { styled } from '@stitches/react'
+import styled from 'styled-components'
 
-export const Container = styled('div', {
-  width: '460px',
-  height: 'auto',
-  // position: 'fixed',
-  display: 'flex',
-  flexDirection: 'row',
-  left: '300px',
-  zIndex: '9999'
-})
+export const Container = styled.div`
+  width: 460px;
+  height: auto;
+  // position: fixed;
+  display: flex;
+  flex-direction: row;
+  left: 300px;
+  z-index: 9999;
+`
 
-export const MovieContainer = styled('div', {
-  width: '860px',
-  height: 'auto',
-  display: 'flex',
+export const MovieContainer = styled.div`
+  width: 860px;
+  height: auto;
+  display: flex;
 
   img: {
-    width: '84px',
-    height: '84px'
+    width: 84px;
+    height: 84px;
   }
-})
+`
